dcd3dc5d833fa7e76d1224b408b82c0ce01ae7be
FrontRowWithJ <adebusum@tcd.ie>
2019-08-08 21:55:53 +0100

    1) Removed unecessary import numpy 2) Added instance variables cx and cy. 3) Added private function "_julia" that generatest the julia matrix using stored instance variables.

--- JuliaPlayground.py
+++ JuliaPlayground.py
@@ -1,13 +1,15 @@
 """Wrapper function for the Julia module."""
-import numpy
-from Julia import *
+import Julia
+
 
 class JuliaPlayground:
     """Class wrapper functions for the Julia module."""
 
     def __init__(self,
-                 width,
-                 height,
+                 width=0,
+                 height=0,
+                 cx=0,
+                 cy=0,
                  xmin_mandel=-2.0,
                  xmax_mandel=1.0,
                  ymin_mandel=-1.3,
@@ -22,6 +24,8 @@ class JuliaPlayground:
         """Initialise the wrapper functions with variables for the julia and mandelbrot set."""
         self.width = width
         self.height = height
+        self.cx = cx
+        self.cy = cy
         self.xmin_mandel = xmin_mandel
         self.xmax_mandel = xmax_mandel
         self.ymin_mandel = ymin_mandel
@@ -36,46 +40,51 @@ class JuliaPlayground:
 
     def mandelbrot(self):
         """Return the mandelbrot set as a 2-d numpy matrix."""
-        return gen_mandelbrot(self.width, self.height, self.xmin_mandel, self.xmax_mandel, self.ymin_mandel, self.ymax_mandel, self.zExponent, self.niter, self.bound)
+        return Julia.gen_mandelbrot(self.width, self.height, self.xmin_mandel, self.xmax_mandel, self.ymin_mandel, self.ymax_mandel, self.zExponent, self.niter, self.bound)
+
+    def _julia(self):
+        return Julia.gen_julia_set(self.width, self.height, self.cx, self.cy, self.xmin_julia, self.xmax_julia, self.ymin_julia, self.ymax_julia, self.zExponent, self.niter, self.bound)
 
     def julia(self, cx, cy):
         """Return the julia set as a 2-d numpy matrix where c = complex(cx, cy)."""
-        return gen_julia_set(self.width, self.height, cx, cy, self.xmin_julia, self.xmax_julia, self.ymin_julia, self.ymax_julia, self.zExponent, self.niter, self.bound)
+        self.cx = cx
+        self.cy = cy
+        return self._julia()
 
     def update_zExponent(self, zExponent):
         """Return a list with the mandelbrot and julia set, updated with a new zExponent."""
         self.zExponent = zExponent
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_niter(self, niter):
         """Return a list with the mandelbrot and julia set, updated with a new niter."""
         self.niter = niter
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_bound(self, bound):
         """Return a list with the mandelbrot and julia set, updated with a new bound."""
         self.bound = bound
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_xmin_mandel(self, xmin_mandel):
         """Return a list with the mandelbrot and julia set, with a new xmin for the mandelbrot set."""
         self.xmin_mandel = xmin_mandel
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_xmax_mandel(self, xmax_mandel):
         """Return a list with the mandelbrot and julia set, with a new xmax for the mandelbrot set."""
         self.xmax_mandel = xmax_mandel
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_ymin_mandel(self, ymin_mandel):
         """Return a list with the mandelbrot and julia set, with a new ymin for the mandelbrot set."""
         self.ymin_mandel = ymin_mandel
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def update_ymax_mandel(self, ymax_mandel):
         """Return a list with the mandelbrot and julia set, with a new ymax for the mandelbrot set."""
         self.ymax_mandel = ymax_mandel
-        return [self.mandelbrot(), self.julia()]
+        return [self.mandelbrot(), self._julia()]
 
     def set_width(self, width):
         """Set the number of colors per row."""
